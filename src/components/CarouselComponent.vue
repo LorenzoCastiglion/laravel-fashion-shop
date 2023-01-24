930a88e8dec1a215d79110ae209cc9cbe7fcdf22
Saimon397 <simonecarrabba@gmail.com>
2023-01-24 23:57:29 +0100

    Add_Carosello

---
+++ src/components/CarouselComponent.vue
@@ -0,0 +1,102 @@
+<template>
+    <carousel :itemsToShow="3.95" :wrapAround="true" :transition="5000" :autoplay="3000">
+        <slide v-for="(slide, index) in homePaths" :key="index">
+            <img class="carousel__item" :src="slide.path" :alt="slide.alt">
+        </slide>
+
+        <template #addons>
+            <navigation />
+            <pagination />
+        </template>
+    </carousel>
+</template>
+
+<script>
+// If you are using PurgeCSS, make sure to whitelist the carousel CSS classes
+import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
+
+
+export default {
+    name: 'CarouselComponent',
+    components: {
+        Carousel,
+        Slide,
+        Pagination,
+        Navigation,
+    },
+    data: function () {
+        return {
+
+            homePaths: [
+                {
+                    path: 'img/moisturizer.png',
+                    alt: 'moisturizer',
+                    id: 1,
+                },
+                {
+                    path: 'img/makeup.png',
+                    alt: 'makeup',
+                    id: 2,
+                },
+                {
+                    path: 'img/eyeshadow.png',
+                    alt: 'eyeshadow',
+                    id: 3,
+                },
+                {
+                    path: 'img/eyeliner.png',
+                    alt: 'eyeliner',
+                    id: 4,
+                },
+            ]
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.carousel__slide {
+    padding: 5px;
+}
+
+.carousel__viewport {
+    perspective: 2000px;
+}
+
+.carousel__track {
+    transform-style: preserve-3d;
+}
+
+.carousel__slide--sliding {
+    transition: 0.5s;
+}
+
+.carousel__slide {
+    opacity: 0.9;
+    transform: rotateY(-20deg) scale(0.9);
+}
+
+.carousel__slide--active~.carousel__slide {
+    transform: rotateY(20deg) scale(0.9);
+}
+
+.carousel__slide--prev {
+    opacity: 1;
+    transform: rotateY(-10deg) scale(0.95);
+}
+
+.carousel__slide--next {
+    opacity: 1;
+    transform: rotateY(10deg) scale(0.95);
+}
+
+.carousel__slide--active {
+    opacity: 1;
+    transform: rotateY(0) scale(1.1);
+}
+
+img {
+    width: 150px;
+}
+</style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,19 +1,22 @@
 <template>
-   
-
-   
+    <div>
         <JumboComponent></JumboComponent>
-   
+    </div>
+    <div class="container mt-5 mb-5">
+        <CarouselComponent></CarouselComponent>
+    </div>
 </template>
 
 <script>
+import CarouselComponent from '../components/CarouselComponent.vue';
 import JumboComponent from '../components/JumboComponent.vue';
-    export default {
-        name:'HomePage',
-        components:{
-            JumboComponent,
-        }
+export default {
+    name: 'HomePage',
+    components: {
+        JumboComponent,
+        CarouselComponent
     }
+}
 </script>
 
 <style lang="scss" scoped>
