937b31f6c94355635dda070b9b03fdc1a823981e
“BarrioTropical” <barrio>
2023-01-26 18:09:29 +0100

    Card Team e Carousel About
    Signed-off-by: “BarrioTropical” <“barrio>

---
+++ src/components/CardTeamComponent.vue
@@ -0,0 +1,214 @@
+<template>
+      <div class="container bootdey">
+        <div class="row">
+          <div class="col-12 text-center">
+              <div class="section-title mb-4 pb-2 mt-4">
+                    <h4 class="title mb-4 text-white">Dream Team</h4>
+                    <p class="text-muted para-desc mx-auto mb-0">Build responsive, mobile-first projects on the web with the world's most popular front-end component library.</p>
+                </div>
+            </div>
+        </div>
+        <div class="row d-flex justify-content-center">
+          <div class="col-lg-2 col-md-6 col-12 mt-4 pt-2"  v-for="(team, index) in teams" :key="index">
+            <div class="team text-center rounded p-3 py-4">
+              <div>
+                <img :src="team.img" class="img-fluid avatar avatar-medium shadow rounded-pill" alt="">
+                  <div class="content mt-3">
+                    <h4 class="title mb-0">{{team.name}}</h4>
+                    <small class="text-muted">{{team.work}}</small>
+                      <ul class="list-unstyled mt-3 social-icon social mb-0">
+                        <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-facebook" title="Facebook"></i></a></li>
+                        <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-instagram" title="Instagram"></i></a></li>
+                        <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-google-plus" title="Google +"></i></a></li>
+                        <li class="list-inline-item"><a href="javascript:void(0)" class="rounded"><i class="mdi mdi-linkedin" title="Linkedin"></i></a></li>
+                    </ul>
+                  </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    
+    
+    
+        
+    </template>
+    
+    <script>
+        export default {
+            
+            name:'CardTeamComponent',
+    
+            components:{
+
+            },
+    
+            
+            data: function() {
+                return{
+                    teams: [
+                    {
+                        id: 1,
+                        img: 'img/logo.png',
+                        name: 'Alessio Bonadonna',
+                        work: 'Junior Developer',
+                    },
+                    {   
+                        id: 2,
+                        img: 'img/logo.png',
+                        name: 'Angelo Calabrò',
+                        work: 'Junior Developer',
+                    },
+                    {
+                        img: 'img/logo.png',
+                        name: 'Simone Carrabba',
+                        work: 'Junior Developer',
+                    },
+                    {
+                        img: 'img/logo.png',
+                        name: 'Lorenzo Castiglion',
+                        work: 'Junior Developer',
+                    },
+                    {
+                        img: 'img/logo.png',
+                        name: 'Simone Conti',
+                        work: 'Junior Developer',
+                    }
+                ]
+                }
+            },
+    
+    
+            method:{
+    
+            }
+        }
+    </script>
+    
+    <style lang="scss" scoped>
+    .box{
+        height: 700px;
+        
+    }
+    
+    body{margin-top:20px;}
+    
+    .team-list img {
+      width: 50%;
+    }
+    
+    .team-list .content {
+      width: 50%;
+    }
+    
+    .team-list .content .follow {
+      position: absolute;
+      bottom: 24px;
+    }
+    
+    .team-list:hover {
+      -webkit-transform: scale(1.05);
+              transform: scale(1.05);
+    }
+    
+    .team, .team-list {
+      -webkit-transition: all 0.5s cubic-bezier(0.68, -0.55, 0.27, 1.55) 0s;
+      transition: all 0.5s cubic-bezier(0.68, -0.55, 0.27, 1.55) 0s;
+    }
+    
+    .team .content .title, .team-list .content .title {
+      font-size: 18px;
+    }
+    
+    .team .overlay {
+      position: absolute;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      opacity: 0;
+      -webkit-transition: all 0.5s ease;
+      transition: all 0.5s ease;
+    }
+    
+    .team .member-position, .team .team-social {
+      position: absolute;
+      bottom: -35px;
+      right: 0;
+      left: 0;
+      margin: auto 10%;
+      z-index: 99;
+    }
+    
+    .team .team-social {
+      bottom: 40px;
+      opacity: 0;
+      -webkit-transition: all 0.5s cubic-bezier(0.68, -0.55, 0.27, 1.55) 0s;
+      transition: all 0.5s cubic-bezier(0.68, -0.55, 0.27, 1.55) 0s;
+    }
+    
+    .team:hover {
+      -webkit-transform: translateY(-7px);
+              transform: translateY(-7px);
+      -webkit-box-shadow: 0 0 5px rgba(0, 0, 0, 0.15);
+              box-shadow: 0 0 5px rgba(0, 0, 0, 0.15);
+    }
+    
+    .team:hover .overlay {
+      opacity: 0.6;
+    }
+    
+    .team:hover .team-social {
+      opacity: 1;
+    }
+    
+    @media (max-width: 768px) {
+      .team-list img,
+      .team-list .content {
+        width: 100%;
+        float: none !important;
+      }
+      .team-list img .follow,
+      .team-list .content .follow {
+        position: relative;
+        bottom: 0;
+      }
+    }
+    
+    .social-icon.social li a {
+        color: #adb5bd;
+        border-color: #adb5bd;
+    }
+    .social-icon li a {
+        color: #35404e;
+        border: 1px solid #35404e;
+        display: inline-block;
+        height: 32px;
+        text-align: center;
+        font-size: 15px;
+        width: 32px;
+        line-height: 30px;
+        -webkit-transition: all 0.4s ease;
+        transition: all 0.4s ease;
+        overflow: hidden;
+        position: relative;
+    }
+    .rounded {
+        border-radius: 5px !important;
+    }
+    
+    .para-desc {
+        max-width: 600px;
+    }
+    .text-muted {
+        color: #8492a6 !important;
+    }
+    
+    .section-title .title {
+        letter-spacing: 0.5px;
+        font-size: 30px;
+    }
+    
+    
+    
+    </style>
---
+++ src/components/CarouselAbout.vue
@@ -0,0 +1,129 @@
+<template>
+    <div class="row">
+          <div class="col-12 text-center">
+              <div class="section-title mb-4 pb-2">
+                    <h4 class="title mb-4 text-white">Our main sponsor</h4>
+                    
+                </div>
+            </div>
+        </div>
+    <div class="container mb-5">
+    <carousel :itemsToShow="3.95" :wrapAround="true" :transition="5000" :autoplay="3000">
+        <slide v-for="(slide, index) in homePaths" :key="index">
+            <div class="col-6">
+                <img class="carousel__item" :src="slide.path" :alt="slide.alt">
+            </div>
+        </slide>
+
+        <template #addons>
+            <navigation class="text-white" />
+        </template>
+    </carousel>
+    </div>
+</template>
+
+<script>
+// If you are using PurgeCSS, make sure to whitelist the carousel CSS classes
+import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
+
+
+export default {
+    name: 'CarouselAbout',
+    components: {
+        Carousel,
+        Slide,
+        Pagination,
+        Navigation,
+    },
+    data: function () {
+        return {
+
+            homePaths: [
+                {
+                    path: 'img/boolean.png',
+                    alt: 'boolean',
+                    info: 'makeup',
+                    id: 1,
+                },
+                {
+                    path: 'img/dior.png',
+                    alt: 'dior',
+                    info: 'makeup',
+                    id: 2,
+                },
+                {
+                    path: 'img/douglas.png',
+                    alt: 'douglas',
+                    info: 'makeup',
+                    id: 3,
+                },
+                {
+                    path: 'img/kiko-milano.png',
+                    alt: 'Kiko Milano',
+                    info: 'makeup',
+                    id: 4,
+                },
+                {
+                    path: 'img/mac.png',
+                    alt: 'Mac',
+                    info: 'makeup',
+                    id: 5,
+                },
+                {
+                    path: 'img/sephora.png',
+                    alt: 'Sephora',
+                    info: 'makeup',
+                    id: 6,
+                },
+            ]
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.carousel__slide {
+    padding: 5px;
+}
+
+.carousel__viewport {
+    perspective: 2000px;
+}
+
+.carousel__track {
+    transform-style: preserve-3d;
+}
+
+.carousel__slide--sliding {
+    transition: 0.5s;
+}
+
+.carousel__slide {
+    opacity: 0.9;
+    transform: rotateY(-20deg) scale(0.9);
+}
+
+.carousel__slide--active~.carousel__slide {
+    transform: rotateY(20deg) scale(0.9);
+}
+
+.carousel__slide--prev {
+    opacity: 1;
+    transform: rotateY(-10deg) scale(0.95);
+}
+
+.carousel__slide--next {
+    opacity: 1;
+    transform: rotateY(10deg) scale(0.95);
+}
+
+.carousel__slide--active {
+    opacity: 1;
+    transform: rotateY(0) scale(1.1);
+}
+
+img {
+    width: 120px;
+}
+</style>
