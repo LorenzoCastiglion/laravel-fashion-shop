0ebcd3b451010808c0e203ae0079b4ee52aa3481
AlessioBonadonna <alessiobonadonna23@gmail.com>
2023-01-27 19:13:59 +0100

    select solo per i tipi esistenti

--- src/pages/Store.vue
+++ src/pages/Store.vue
@@ -1,59 +1,109 @@
 <template>
-    <div class="container m-auto">
-        <div class=" cards-container overflow-auto">
-            <div class="d-flex justify-content-around gap-5  flex-wrap">
-                <CardComponent v-for="(item, index) in products" :key="index" :card="item"></CardComponent>
-            </div>
-        </div>
+  <div class="container m-auto">
+    <!-- SELECT PER TIPO-->
+    <div>
+      <select v-model="selectedType" @change="getProductsByType">
+        <option v-for="(type, index) in types" :key="index" :value="type.id">
+          {{ type.name }}
+        </option>
+      </select>
     </div>
+
+    <div class="cards-container overflow-auto">
+      <div class="d-flex justify-content-around gap-5 flex-wrap">
+        <CardComponent
+          v-for="(item, index) in products"
+          :key="index"
+          :card="item"
+        ></CardComponent>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-
-import axios from 'axios';
-import { store } from '../store';
-import CardComponent from '../components/CardComponent.vue';
+import axios from "axios";
+import { store } from "../store";
+import CardComponent from "../components/CardComponent.vue";
+import { extractIdentifiers } from "@vue/compiler-core";
 
 export default {
-    name: 'Store',
+  name: "Store",
 
-    components: {
-        CardComponent
+  components: {
+    CardComponent,
+  },
+  data() {
+    return {
+      store,
+      products: null,
+      selectedType: null,
+      types: [],
+    };
+  },
+  created(){
+    this.getTypes();
+  },
+
+  methods: {
+    getProduct() {
+      axios.get(`${this.store.apiBaseUrl}/products`).then((response) => {
+        console.log(response.data.results);
+        this.products = response.data.results;
+        this.currentPage = response.data.results.current_page;
+        this.lastPage = response.data.results.last_page;
+        this.total = response.data.results.total;
+      });
     },
-    data() {
-        return {
-            store,
-            products: null,
-
-        }
+    getProductsByType() {
+      if (this.selectedType) {
+        console.log(`http://127.0.0.1:8000/api/products?typeFilter=${this.selectedType}`);
+        axios
+          .get(
+            `http://127.0.0.1:8000/api/products?typeFilter=${this.selectedType}`
+          )
+          .then((response) => {
+            console.log(response);
+            console.log(response.data.results);
+            this.products = response.data.results;
+          });
+      }
     },
+    getTypes() {
+        let types_ = [];
+        let check = false;
+        //console.log("ciao")
+            axios.get(`http://127.0.0.1:8000/api/products?typeFilter=`)
+                .then(response => {
+                    console.log("sono in risposta")
+                    console.log(response.data.results);
+                    response.data.results.forEach(type => {
+                        types_.forEach(element => {
+                            if(element.name == type.type.name){
+                                check = true;
+                            }
+                        });
 
-    methods: {
-        getProduct() {
+                        if(!check){
+                        types_.push(type.type);
+                        }
 
-            axios.get(`${this.store.apiBaseUrl}/products`).then((response) => {
-                console.log(response.data.results) 
-                this.products = response.data.results;
-                this.currentPage = response.data.results.current_page;
-                this.lastPage = response.data.results.last_page;
-                this.total = response.data.results.total;
-            })
+                        check = false;
+                    });
+                    this.types = types_;
+                    console.log(this.types);
+                });
         },
-    },
+  },
 
-    mounted() {
-
-        this.getProduct()
-    }
-
-
-}
-
-
+  mounted() {
+    this.getProduct();
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 .cards-container {
-    height: 65vh;
+  height: 65vh;
 }
 </style>
