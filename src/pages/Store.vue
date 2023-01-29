78f0edc7200f76a70020b4bc2bac5dc2fc03ae31
AlessioBonadonna <alessiobonadonna23@gmail.com>
2023-01-29 12:17:40 +0100

    add filter complete

--- src/pages/Store.vue
+++ src/pages/Store.vue
@@ -2,21 +2,44 @@
   <div class="container m-auto">
     <!-- SELECT PER TIPO-->
     <div>
-      <select v-model="selectedType" @change="getProductsByType">
+      <!-- @change="getProductsByType"> -->
+      <select v-model="selectedType" >
         <option v-for="(type, index) in types" :key="index" :value="type.id">
           {{ type.name }}
         </option>
       </select>
+
+      <!-- SELECT PER BRAND-->
+      <!-- @change="getProductsByBrands"> -->
+      <select v-model="selectedBrand" >
+        <option v-for="(brand, index) in brands" :key="index" :value="brand.id">
+          {{ brand.name }}
+        </option>
+      </select>
+     <!-- select per texture-->
+     <!-- <select v-model="selectedTexture" @change="getProductsByTextures">
+        <option v-for="(texture, index) in textures" :key="index" :value="texture.id">
+          {{ texture.name }}
+        </option>
+      </select> -->
+      <button @click="getProduct()">FILTRA</button>
+
+
     </div>
 
     <div class="cards-container overflow-auto">
-      <div class="d-flex justify-content-around gap-5 flex-wrap">
+      <div v-if="products.length == 0">
+        prodotti non disponibili
+      </div>
+      <div class="d-flex justify-content-around gap-5 flex-wrap " v-else-if="products ">
         <CardComponent
           v-for="(item, index) in products"
           :key="index"
-          :card="item"
+          :card="item" 
         ></CardComponent>
+        
       </div>
+     
     </div>
   </div>
 </template>
@@ -36,31 +59,65 @@ export default {
   data() {
     return {
       store,
-      products: null,
+      products: [],
       selectedType: null,
+      selectedBrand: null,
+      selectedTexture: null,
       types: [],
+      brands: [],
+      textures: [],
     };
   },
-  created(){
+  created() {
     this.getTypes();
+    this.getBrands();
+    //this.getTextures();
   },
 
   methods: {
     getProduct() {
-      axios.get(`${this.store.apiBaseUrl}/products`).then((response) => {
+      let data={
+        params:{
+          typeFilter:this.selectedType,
+          brandFilter:this.selectedBrand,
+          
+        }
+
+      }
+      axios.get(`${this.store.apiBaseUrl}/products`,data).then((response) => {
         console.log(response.data.results);
         this.products = response.data.results;
         this.currentPage = response.data.results.current_page;
         this.lastPage = response.data.results.last_page;
         this.total = response.data.results.total;
+        
       });
     },
     getProductsByType() {
       if (this.selectedType) {
-        console.log(`http://127.0.0.1:8000/api/products?typeFilter=${this.selectedType}`);
+        console.log(
+          `http://127.0.0.1:8000/api/products?typeFilter=${this.selectedType}`
+        );
         axios
           .get(
-            `http://127.0.0.1:8000/api/products?typeFilter=${this.selectedType}`
+           `http://127.0.0.1:8000/api/products?typeFilter=${this.selectedType}`
+         )
+          .then((response) => {
+           // console.log(response);
+           // console.log(response.data.results);
+            this.products = response.data.results;
+          });
+      }
+    },
+    getProductsByBrands() {
+      console.log("ciao");
+      if (this.selectedBrand) {
+       // console.log(
+        //  `http://127.0.0.1:8000/api/products?brandFilter=${this.selectedBrand}`
+       // );
+        axios
+          .get(
+           `http://127.0.0.1:8000/api/products?brandFilter=${this.selectedBrand}`
           )
           .then((response) => {
             console.log(response);
@@ -69,31 +126,114 @@ export default {
           });
       }
     },
+    // getProductsByTextures() {
+    //   console.log("ciao");
+    //   if (this.selectedTexture) {
+    //    // console.log(
+    //      // `http://127.0.0.1:8000/api/products?textureFilter=${this.selectedTexture}`
+    //    // );
+    //     axios
+    //       .get(
+    //         `http://127.0.0.1:8000/api/products?textureFilter=${this.selectedTexture}`
+    //       )
+    //       .then((response) => {
+    //         //console.log(response);
+    //        // console.log(response.data.results);
+    //         this.products = response.data.results;
+    //       });
+    //   }
+    // },
+    // getProductsByTextures() {
+    //   console.log("ciao");
+    //   if (this.selectedTexture) {
+    //     console.log(
+    //       `http://127.0.0.1:8000/api/products?textureFilter=${this.selectedtexture}`
+    //     );
+    //     axios
+    //       .get(
+    //         `http://127.0.0.1:8000/api/products?textureFilter=${this.selectedtexture}`
+    //       )
+    //       .then((response) => {
+    //         console.log(response);
+    //         console.log(response.data.results);
+    //         this.products = response.data.results;
+    //       });
+    //   }
+    // },
+
     getTypes() {
-        let types_ = [];
-        let check = false;
-        //console.log("ciao")
-            axios.get(`http://127.0.0.1:8000/api/products?typeFilter=`)
-                .then(response => {
-                    console.log("sono in risposta")
-                    console.log(response.data.results);
-                    response.data.results.forEach(type => {
-                        types_.forEach(element => {
-                            if(element.name == type.type.name){
-                                check = true;
-                            }
-                        });
+      let types_ = [];
+      let check = false;
+      //console.log("ciao")
+      axios.get(`http://127.0.0.1:8000/api/products`).then((response) => {
+       // console.log("sono in risposta");
+       // console.log(response.data.results);
+        response.data.results.forEach((type) => {
+          types_.forEach((element) => {
+            if (element.name == type.type.name) {
+              check = true;
+            }
+          });
 
-                        if(!check){
-                        types_.push(type.type);
-                        }
+          if (!check) {
+            types_.push(type.type);
+          }
 
-                        check = false;
-                    });
-                    this.types = types_;
-                    console.log(this.types);
-                });
-        },
+          check = false;
+        });
+        this.types = types_;
+       // console.log(this.types);
+      });
+    },
+    getBrands() {
+      let brands_ = [];
+      let check = false;
+      //console.log("ciao")
+      axios.get(`http://127.0.0.1:8000/api/products`).then((response) => {
+       // console.log("sono in risposta");
+       // console.log(response.data.results);
+        response.data.results.forEach((brand) => {
+          brands_.forEach((element) => {
+            if (element.name == brand.brand.name) {
+              check = true;
+            }
+          });
+
+          if (!check) {
+            brands_.push(brand.brand);
+          }
+
+          check = false;
+        });
+        this.brands = brands_;
+      });
+    },
+    getTextures() {
+      let textures_ = [];
+      let check = false;
+      //console.log("ciao")
+      axios.get(`http://127.0.0.1:8000/api/products`).then((response) => {
+        console.log("sono in risposta");
+
+        console.log(response.data.results);
+        response.data.results.forEach((texture) => {
+           // console.log(texture)
+          textures_.forEach((element) => {
+            if (element.name == texture.texture.name) {
+              check = true;
+            }
+          });
+
+          if (!check) {
+            textures_.push(texture.texture);
+          }
+
+          check = false;
+        });
+        this.textures = textures_;
+      });
+    },
+  
   },
 
   mounted() {
